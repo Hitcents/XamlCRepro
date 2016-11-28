76dd18da2cb608d72c2f959a328a564d9fd8a8aa
Jonathan Peppers <jonathan.peppers@hitcents.com>
2016-11-28 09:43:22 -0600

    Refactored repro to show attached property issue

---
+++ XamlCRepro/XamlCRepro/Theme.cs
@@ -0,0 +1,31 @@
+﻿using Xamarin.Forms;
+
+namespace XamlCRepro
+{
+    public class Theme
+    {
+        public static readonly BindableProperty StartColorProperty = BindableProperty.CreateAttached("StartColor", typeof(Color), typeof(Theme), Color.Transparent);
+
+        public static readonly BindableProperty EndColorProperty = BindableProperty.CreateAttached("EndColor", typeof(Color), typeof(Theme), Color.Transparent);
+
+        public static Color GetStartColor(BindableObject obj)
+        {
+            return (Color)obj.GetValue(StartColorProperty);
+        }
+
+        public static void SetStartColor(BindableObject obj, Color value)
+        {
+            obj.SetValue(StartColorProperty, value);
+        }
+
+        public static Color GetEndColor(BindableObject obj)
+        {
+            return (Color)obj.GetValue(EndColorProperty);
+        }
+
+        public static void SetEndColor(BindableObject obj, Color value)
+        {
+            obj.SetValue(EndColorProperty, value);
+        }
+    }
+}
